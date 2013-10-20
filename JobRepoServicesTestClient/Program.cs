aa85c4cf166dbe641d3f902bdeafd5e365592680
Zhinoo <elismidth-puad@yahoo.com>
2013-10-20 23:05:46 +1100

    JobRepo Commit1

---
+++ JobRepoServicesTestClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JobRepoServicesTestClient.SalarySurveyRef;
+
+namespace JobRepoServicesTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SalarySurveyServiceClient client = new SalarySurveyServiceClient();
+
+            foreach (SalarySurvey ss in client.GetAllRoles())
+                Console.WriteLine(ss.Role + "  " + ss.MinSalary + "  " + ss.MaxSalary + "  " + ss.AverageSalary);
+
+            Console.ReadLine();
+            // Always close the client.
+            client.Close();
+        }
+    }
+}
